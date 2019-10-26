3588b10af5286c98d2ff472988da24a2772bd9e2
Johnathan Bressette <bressetteprogramming@gmail.com>
2019-10-26 15:03:20 -0400

    Initial commit

---
+++ learningPython/helloWorld.py
@@ -0,0 +1,12 @@
+
+num1 = 5
+num2 = 10
+num3 = 15
+testString = "This is a string that does stuff"
+newString = "This is also a string that does stuff"
+
+print("Enter a value")
+userInput = input()
+print("The user entered ", userInput)
+print ("Hello World", num1, num2, num3)
+print(testString + " " + newString)
