bdb1b3f495bb315b8724b305b67216de5a14249e
Johnathan Bressette <bressetteprogramming@gmail.com>
2019-10-29 09:14:09 -0400

    Add reset index to parseCSV to correct ID values
    The reset index function was used after every drop duplicates call
    to reset the index to be used for table IDs so that the IDs are
    incremental integer values.
    
    Reset index was necessary to output the correct ID for each table
    as before after the duplicate rows are droppped the index values stay
    the same as what was in the original table and are not incremental.

--- crashDatabaseProject/parseCSV.py
+++ crashDatabaseProject/parseCSV.py
@@ -7,15 +7,18 @@ print(list(df))
 df_weather = df[['Weather', 'SurfaceCondition', 'DayNight']]
 print(df_weather)
 df_weather.drop_duplicates(keep='first', inplace=True)
+df_weather.reset_index(inplace=True)
 print(df_weather)
 df_weather.to_csv("Weather CSV.csv")
 
 df_location = df[['STREETADDRESS', 'CITYORTOWN', 'RoadCharacteristics']]
 df_location.drop_duplicates(keep='first', inplace=True)
+df_location.reset_index(inplace=True)
 df_location.to_csv("location.csv")
 
 df_driver = df[['Impairment', 'InjuryType']]
 df_driver.drop_duplicates(keep='first', inplace=True)
+df_driver.reset_index(inplace=True)
 print(df_driver)
 df_driver.to_csv("driver.csv")
 
