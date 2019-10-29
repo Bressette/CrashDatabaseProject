c5e64e3f5582581a249abe89cdac0cddf49197d6
Johnathan Bressette <bressetteprogramming@gmail.com>
2019-10-28 22:44:24 -0400

    Add location and driver CSV files

--- crashDatabaseProject/parseCSV.py
+++ crashDatabaseProject/parseCSV.py
@@ -10,13 +10,11 @@ df_weather.drop_duplicates(keep='first', inplace=True)
 print(df_weather)
 df_weather.to_csv("Weather CSV.csv")
 
-df_location = df[['STREETADDRESS']]
+df_location = df[['STREETADDRESS', 'CITYORTOWN', 'RoadCharacteristics']]
 df_location.drop_duplicates(keep='first', inplace=True)
-
-df_location.to_csv("droppedAddresses.csv")
+df_location.to_csv("location.csv")
 
 df_driver = df[['Impairment', 'InjuryType']]
-#print(df_driver)
 df_driver.drop_duplicates(keep='first', inplace=True)
 print(df_driver)
 df_driver.to_csv("driver.csv")
