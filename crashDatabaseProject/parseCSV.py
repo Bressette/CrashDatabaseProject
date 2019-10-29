71ffb89088bbd2e217851ec8ffc9c77ed078078a
Johnathan Bressette <bressetteprogramming@gmail.com>
2019-10-28 22:26:13 -0400

    Restructure script to output correct Weather CSV

--- crashDatabaseProject/parseCSV.py
+++ crashDatabaseProject/parseCSV.py
@@ -6,14 +6,19 @@ print(df)
 print(list(df))
 df_weather = df[['Weather', 'SurfaceCondition', 'DayNight']]
 print(df_weather)
-df_weather.drop_duplicates(keep=False, inplace=True)
+df_weather.drop_duplicates(keep='first', inplace=True)
 print(df_weather)
-#df_weather.to_csv("Weather CSV.csv")
+df_weather.to_csv("Weather CSV.csv")
 
 df_location = df[['STREETADDRESS']]
-df_location.drop_duplicates(keep=False, inplace=True)
-#df_location.to_csv("droppedAddresses.csv")
+df_location.drop_duplicates(keep='first', inplace=True)
 
+df_location.to_csv("droppedAddresses.csv")
 
+df_driver = df[['Impairment', 'InjuryType']]
+#print(df_driver)
+df_driver.drop_duplicates(keep='first', inplace=True)
+print(df_driver)
+df_driver.to_csv("driver.csv")
 
 #df.to_csv("Empty CSV.csv")
