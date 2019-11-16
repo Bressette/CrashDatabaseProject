fff2b78534a828528500542dd5c98ab1b7eccfd7
Johnathan Bressette <bressetteprogramming@gmail.com>
2019-11-15 23:39:07 -0500

    Add comments to sanitizeTables.py

--- crashDatabaseProject/sanitizeTables.py
+++ crashDatabaseProject/sanitizeTables.py
@@ -13,6 +13,8 @@ def get_time(s):
     return s
 
 
+#function that formats the location by dropping invalid values
+#and renaming the fields to match the database schema
 def format_location(mergedCity):
     mergedCity.drop(['CITYORTOWN'], axis=1, inplace=True)
     mergedCity = mergedCity.rename(columns={"STREETADDRESS": "streetAddress", "RoadCharacteristics": "roadChar"})
@@ -23,7 +25,7 @@ def format_location(mergedCity):
     mergedCity["streetAddress"] = mergedCity["streetAddress"].fillna("Not Reported")
     return mergedCity
 
-
+#function that replaces invalid values in the collisionDir field in accident
 def sanitize_collision_dir(df):
     df['collisionDir'] = df['collisionDir'].replace(to_replace="No Turns, Thru moves only, Broadside ^<",
                                                     value="No Turns, Thru moves only, Broadside")
@@ -47,7 +49,7 @@ def sanitize_collision_dir(df):
                                                     value="Right Turn, Same Direction, Rear End")
     return df
 
-
+#function that sanitizes weather by replacing invalid values
 def sanitize_weather(df_weather):
     df_weather['surfaceCond'] = df_weather['surfaceCond'].replace(to_replace="Not Reported", value="Unknown")
     df_weather['surfaceCond'] = df_weather['surfaceCond'].replace(to_replace="Other - Explain in Narrative",
@@ -56,13 +58,14 @@ def sanitize_weather(df_weather):
     df_weather['dayNight'] = df_weather['dayNight'].fillna("Unknown")
     return df_weather
 
-
+#function that fills nulls in driver
 def sanitize_driver(df_driver):
     df_driver['driverImpair'] = df_driver['driverImpair'].fillna("None")
     df_driver['driverDamage'] = df_driver['driverDamage'].fillna("Unknown")
     return df_driver
 
 
+#function that merges the city and address table to give address the foreign key from city
 def merge_city_address(df_location, df_city, df_address):
     mergedCity = pandas.merge(df_location, df_city, how='left', left_on=['CITYORTOWN'], right_on=['CITYORTOWN'])
     mergedCity = format_location(mergedCity)
